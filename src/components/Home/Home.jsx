639a3358e48f254d6bdef893ecf10af7680d81d1
Siam-al-islam <siamalislam692@gmail.com>
2024-05-17 21:42:36 +0600

    loader added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Header from "./Header/Header";
 import Footer from "../Footer/Footer";
 
 const Home = () => {
+
+    const navigation = useNavigation();
+    const location = useLocation();
     return (
         <div>
             <Header />
+            {
+                navigation.state === "loading" ? <p>Loading ...</p> : <Outlet />
+            }
             <h2>Home page</h2>
-
-            <Outlet />
             <Footer />
         </div>
     );
